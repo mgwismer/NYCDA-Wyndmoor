dcd99cf4ad547fd46158f5e922c2981ebd8d4e7e
Margaret Wismer <margaretgwismer.gmail.com>
2016-12-02 17:14:31 -0500

    modal on hover

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+// $(document).ready(function(){
+//     $( ".main-container" ).hover(function() {
+//            $('.nature-modal').modal({
+//         show: false;
+//     });
+//     $( ".nature-image" ).hover(function() {
+//            $('.modal').modal({
+//         show: true;
+//     });
+//   });  
+// });
+
 $(document).ready(function(){
     $( ".nature-image" ).hover(function() {
            $('.modal').modal({
